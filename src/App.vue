292d45e5e0ae751489eb1f401b90d80c0e8538be
Pedro Soares Lima Vieira <pedroslvieira@gmail.com>
2022-07-20 21:21:13 +0200

    added links to exercises

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<!-- Links to original repo, exercises and slides -->
+<!-- https://vueschool.notion.site/Exercises-d0f485e569464b949c081208df12b3d9 -->
+<!-- https://slides.com/danielkelly-2/vue-3-and-the-composition-api-workshop#/40/0/2 -->
+<!-- https://github.com/vueschool/workshop-vue-3-composition-api/tree/main -->
 <template>
   <div id="app">
     <!-- <HelloWorld msg="Rick & Morty Characters" /> -->
